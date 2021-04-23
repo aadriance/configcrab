6e3d8009f594deab667d390d52e5cbcbd0e280c8
aadriance <primaryartemis@outlook.com>
2021-04-22 19:42:38 -0700

    Play with config initialization

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,43 @@ struct Config {
     linuxpath: String,
 }
 
+impl Config {
+    fn new() -> Config {
+        Config {
+            file: "".to_string(),
+            winpath: "".to_string(),
+            macpath: "".to_string(),
+            linuxpath: "".to_string()
+        }
+    }
+
+    fn with_file(mut self, file: &str) -> Self {
+        self.file = file.to_string();
+        self
+    }
+
+    fn with_winpath(mut self, winpath: &str) -> Self {
+        self.winpath = winpath.to_string();
+        self
+    }
+
+    fn with_macpath(mut self, macpath: &str) -> Self {
+        self.macpath = macpath.to_string();
+        self
+    }
+
+    fn with_linuxpath(mut self, linuxpath: &str) -> Self {
+        self.linuxpath = linuxpath.to_string();
+        self
+    }
+}
+
+#[derive(Debug)]
+struct CrabOrders {
+    config_path: String,
+    platform: String
+}
+
 fn main() {
     let matches = App::new("ConfigCrab")
         .version("0.1.0")
@@ -33,13 +70,17 @@ fn main() {
     });
 
     //eg; macos windows etc.
-    println!("You're running: {}", env::consts::OS);
-    let config = Config {
-        file: "Test".to_string(),
-        winpath: "TestWin".to_string(),
-        macpath: "TestMac".to_string(),
-        linuxpath: "TestLinux".to_string(),
+    let orders = CrabOrders {
+        config_path: "configcrab.yaml".to_string(),
+        platform: env::consts::OS.to_string()
     };
+
+    println!("Your orders: {:?}", orders);
+    let config = Config::new()
+        .with_file("file")
+        .with_linuxpath("linux")
+        .with_macpath("mac")
+        .with_winpath("win");
 
     let example_config = [config.clone(), config];
     export_config(&example_config, "configcrab.yaml").unwrap();
@@ -57,4 +98,25 @@ fn import_config(file: &str) -> Result<Vec<Config>> {
     let config_yaml = fs::read_to_string(file)?;
     let config: Vec<Config> = serde_yaml::from_str(&config_yaml)?;
     Ok(config)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_config_init() {
+        let test_config = Config::new();
+        assert_eq!("", test_config.file);
+        let full_config = Config::new()
+            .with_file("file")
+            .with_macpath("macpath")
+            .with_winpath("winpath")
+            .with_linuxpath("linuxpath");
+
+        assert_eq!("file", full_config.file);
+        assert_eq!("macpath", full_config.macpath);
+        assert_eq!("winpath", full_config.winpath);
+        assert_eq!("linuxpath", full_config.linuxpath);
+    }
 }
