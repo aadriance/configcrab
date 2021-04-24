3301a2781851b1cf7d94e5a0cfc0a57140c7660a
aadriance <primaryartemis@outlook.com>
2021-04-23 18:50:20 -0700

    Add in tests to cover functionality
    I've got no functionality, but I know that all my cargo tests pass.  Isn't that functionality in and of itself :smile:

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ struct PlatformPath {
     path: String,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 struct Config {
     file: String,
     paths: Vec<PlatformPath>,
@@ -17,7 +17,7 @@ struct Config {
 impl Config {
     fn new() -> Config {
         Config {
-            file: "file".to_string(),
+            file: "".to_string(),
             paths: Vec::new(),
         }
     }
@@ -78,7 +78,6 @@ fn main() {
         0
     });
 
-    //eg; macos windows etc.
     let orders = CrabOrders {
         config_path: "configcrab.yaml".to_string(),
         platform: env::consts::OS.to_string(),
@@ -109,25 +108,68 @@ fn import_config(file: &str) -> Result<Vec<Config>> {
     Ok(config)
 }
 
-/*
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_config_init() {
+    fn test_config_init_mac() {
+        let test_config = Config::new();
+        assert_eq!("", test_config.file);
+        let full_config = Config::new().with_file("file").with_macpath("macpath");
+
+        assert_eq!("file", full_config.file);
+        assert_eq!("macos", full_config.paths[0].platform);
+        assert_eq!("macpath", full_config.paths[0].path);
+    }
+
+    #[test]
+    fn test_config_init_win() {
+        let test_config = Config::new();
+        assert_eq!("", test_config.file);
+        let full_config = Config::new().with_file("file").with_winpath("winpath");
+
+        assert_eq!("file", full_config.file);
+        assert_eq!("windows", full_config.paths[0].platform);
+        assert_eq!("winpath", full_config.paths[0].path);
+    }
+
+    #[test]
+    fn test_config_init_linux() {
+        let test_config = Config::new();
+        assert_eq!("", test_config.file);
+        let full_config = Config::new().with_file("file").with_linuxpath("linuxpath");
+
+        assert_eq!("file", full_config.file);
+        assert_eq!("linux", full_config.paths[0].platform);
+        assert_eq!("linuxpath", full_config.paths[0].path);
+    }
+
+    #[test]
+    fn test_config_init_platform() {
         let test_config = Config::new();
         assert_eq!("", test_config.file);
         let full_config = Config::new()
             .with_file("file")
-            .with_macpath("macpath")
-            .with_winpath("winpath")
-            .with_linuxpath("linuxpath");
+            .with_platform(env::consts::OS, "platpath");
 
         assert_eq!("file", full_config.file);
-        assert_eq!("macpath", full_config.macpath);
-        assert_eq!("winpath", full_config.winpath);
-        assert_eq!("linuxpath", full_config.linuxpath);
+        assert_eq!(env::consts::OS, full_config.paths[0].platform);
+        assert_eq!("platpath", full_config.paths[0].path);
+    }
+
+    #[test]
+    fn test_import_export() {
+        let config = Config::new()
+            .with_file("file")
+            .with_linuxpath("linux")
+            .with_macpath("mac")
+            .with_winpath("win");
+
+        let example_config = [config.clone(), config];
+        export_config(&example_config, "test_configcrab.yaml").unwrap();
+        let import = import_config("test_configcrab.yaml").unwrap();
+        fs::remove_file("test_configcrab.yaml").unwrap();
+        assert_eq!(example_config.to_vec(), import);
     }
 }
-*/
